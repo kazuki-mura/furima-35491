dded03b7615111ee8e443c5171b5b716682c9147
kazuki-mura <k.mura0131@gmail.com>
2021-06-19 20:29:14 +0900

    Userモデルのテストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
          validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/ }
          validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/}
-         validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/}
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/}
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/}
          validates :birth_date, presence: true
          
 
