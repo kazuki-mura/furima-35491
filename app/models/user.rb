c219d6f86ef8834c6a6517f17fda6bc68ddce5eb
kazuki-mura <k.mura0131@gmail.com>
2021-06-19 14:34:01 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/ }
          validates :nickname, presence: true
          validates :last_name, presence: true
          validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
+         validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/}
+         validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/}
          validates :birth_date, presence: true
          
 
