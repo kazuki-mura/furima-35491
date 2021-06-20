7839a5afff38ac500af0ce2092d2e5765b7b6084
kazuki-mura <k.mura0131@gmail.com>
2021-06-20 15:33:11 +0900

    バリデーション記述の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,16 @@ class User < ApplicationRecord
          
        with_options presence: true do
          validates :nickname
-         validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-         validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-         validates :last_name_kana, format: { with: /\A[ァ-ヶ]+\z/}
-         validates :first_name_kana, format: { with: /\A[ァ-ヶ]+\z/}
          validates :birth_date
+
+         with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+          validates :last_name 
+          validates :first_name
+         end
+
+         with_options  format: { with: /\A[ァ-ヶ]+\z/} do
+          validates :last_name_kana
+          validates :first_name_kana
+         end
        end
 end
