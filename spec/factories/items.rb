ce62d34f15650cc477c7efffd6d70c5c85308cfb
kazuki-mura <k.mura0131@gmail.com>
2021-06-24 22:03:39 +0900

    itemモデルのテストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                      {Faker::Name.initials(number: 2)}
+    info                      {Faker::Text}
+    category_id               {Faker::NumberBetween($min = 1,$max = 11 )}
+    sales_status_id           {Faker::NumberBetween($min = 2,$max = 7 )}
+    price                     {Faker::NumberBetween($min = 300,$max = 9999999 )}
+    shipping_fee_status_id    {Faker::NumberBetween($min = 2,$max = 3 )}
+    prefecture_id             {Faker::NumberBetween($min = 2,$max = 48 )}
+    scheduled_delivery_id     {Faker::NumberBetween($min = 2,$max = 4 )}
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '出品ページへの遷移' do
+
+    context 'ユーザーがログインしている時' do
+      it '出品ページへ遷移できる' do
+      end
+    end
+
+    context 'ユーザーがログインしていない時' do
+      it 'ログイン画面へ遷移する' do
+      end
+    end
+
+  end
+
+  describe '出品の保存' do
+
+    context '出品が投稿できる場合' do
+      it '画像とテキストがあれば投稿できる' do
+      end
+      it 'テキストがあれば投稿できる' do
+      end
+    end
+
+    context 'ツイートが投稿できない場合' do
+      it 'nameがid:1では投稿できない' do
+      end 
+
+      it 'infoがid:1では投稿できない' do
+      end
+
+      it 'category_idがid:1では投稿できない' do
+      end
+
+      it 'sales_status_idがid:1では投稿できない' do
+      end
+
+      it 'priceが空では投稿できない' do
+      end
+
+      it 'priceが300以下では投稿できない' do
+      end
+
+      it 'priceが9999999以上では投稿できない' do
+      end
+
+      it 'priceが大文字では投稿できない' do
+      end
+
+      it 'shipping_fee_status_idがid:1では投稿できない' do
+      end
+
+      it 'prefecture_idがid:1では投稿できない' do
+      end
+
+      it 'scheduled_delivery_idがid:1では投稿できない' do
+      end
+
+      it 'ユーザーが紐付いていなければ投稿できない' do
+      end
+
+
+
+
+
+    end
+  end
+
+
+
 end
