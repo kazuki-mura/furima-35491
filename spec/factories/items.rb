edbee5f580dc1313708dce9c122feb7b93ffb9d6
kazuki-mura <k.mura0131@gmail.com>
2021-06-26 12:25:28 +0900

    itemモデルのテストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,18 @@
 FactoryBot.define do
   factory :item do
     name                      {Faker::Name.initials(number: 2)}
-    info                      {Faker::Text}
-    category_id               {Faker::NumberBetween($min = 1,$max = 11 )}
-    sales_status_id           {Faker::NumberBetween($min = 2,$max = 7 )}
-    price                     {Faker::NumberBetween($min = 300,$max = 9999999 )}
-    shipping_fee_status_id    {Faker::NumberBetween($min = 2,$max = 3 )}
-    prefecture_id             {Faker::NumberBetween($min = 2,$max = 48 )}
-    scheduled_delivery_id     {Faker::NumberBetween($min = 2,$max = 4 )}
+    info                      {Faker::Lorem.sentence}
+    category_id               {Faker::Number.between(from: 2, to: 10)}
+    sales_status_id           {Faker::Number.between(from: 2, to: 7)}
+    price                     {Faker::Number.between(from: 300, to: 9999999)}
+    shipping_fee_status_id    {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id             {Faker::Number.between(from: 2, to: 48)}
+    scheduled_delivery_id     {Faker::Number.between(from: 2, to: 4)}
     association :user
+    
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
+
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,65 +4,100 @@ RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
   end
-
-  describe '出品ページへの遷移' do
-
-    context 'ユーザーがログインしている時' do
-      it '出品ページへ遷移できる' do
-      end
-    end
-
-    context 'ユーザーがログインしていない時' do
-      it 'ログイン画面へ遷移する' do
-      end
-    end
-
-  end
-
-  describe '出品の保存' do
-
+  
+  describe '商品の出品' do
+    
     context '出品が投稿できる場合' do
-      it '画像とテキストがあれば投稿できる' do
+      it '入力項目が正しければ出品ができる' do
+        expect(@item).to be_valid
       end
-      it 'テキストがあれば投稿できる' do
-      end
+     
     end
-
-    context 'ツイートが投稿できない場合' do
-      it 'nameがid:1では投稿できない' do
+    
+    context '出品ができない場合' do
+      
+      it 'imageが空では投稿できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end 
 
-      it 'infoがid:1では投稿できない' do
+      it 'nameが空では投稿できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end 
+
+      it 'infoが空では投稿できない' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
       end
 
       it 'category_idがid:1では投稿できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
 
       it 'sales_status_idがid:1では投稿できない' do
+        @item.sales_status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status must be other than 1")
       end
 
       it 'priceが空では投稿できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
       it 'priceが300以下では投稿できない' do
+        @item.price = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
 
       it 'priceが9999999以上では投稿できない' do
+        @item.price = '100000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
 
-      it 'priceが大文字では投稿できない' do
+      it 'priceが全角数字では投稿できない' do
+        @item.price = '１２３４５'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+
+      it 'priceが数字では投稿できない' do
+        @item.price = '二千九百'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
 
       it 'shipping_fee_status_idがid:1では投稿できない' do
+        @item.shipping_fee_status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee status must be other than 1")
       end
 
       it 'prefecture_idがid:1では投稿できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
 
       it 'scheduled_delivery_idがid:1では投稿できない' do
+        @item.scheduled_delivery_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery must be other than 1")
       end
 
       it 'ユーザーが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
       end
 
 
