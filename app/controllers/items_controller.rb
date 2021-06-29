31c3b272568ca2b956bcd479cc81e917f450669f
kazuki-mura <k.mura0131@gmail.com>
2021-06-29 21:01:26 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update] 
-  before_action :redirect_to_index, only: [:edit, :update]
-  before_action :authenticate_user!, only: [:new, :edit, :update, :create]
+  before_action :redirect_to_index, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :create, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
