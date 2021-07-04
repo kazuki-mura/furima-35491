57e8ca65dac289b30e93beced2bfeda6b7f4a47e
kazuki-mura <k.mura0131@gmail.com>
2021-07-04 17:47:02 +0900

    コードレビューの修正

--- spec/models/order_pay_form_spec.rb
+++ spec/models/order_pay_form_spec.rb
@@ -7,20 +7,27 @@ RSpec.describe OrderPayForm, type: :model do
       @item = FactoryBot.create(:item)
       @order_pay_form = FactoryBot.build(:order_pay_form,user_id: @user.id , item_id: @item.id)
       sleep 0.1
-     end
+    end
     context '商品が購入できる場合' do
       it 'すべての値が正しく入力されていれば保存できること' do
         expect(@order_pay_form).to be_valid
       end
       it '建物番号は空でも登録できること'do
-        @order_pay_form.building = ' '
-        expect(@order_pay_form).to be_valid
-      end
+      @order_pay_form.building = ' '
+      expect(@order_pay_form).to be_valid
+    end
+  end
+  
+  context '商品が購入できない場合' do
+
+    it "tokenが空では登録できないこと" do
+      @order_pay_form.token = nil
+      @order_pay_form.valid?
+      expect(@order_pay_form.errors.full_messages).to include("Token can't be blank")
     end
 
-    context '商品が購入できない場合' do
-      it 'user_idが空である' do
-        @order_pay_form.user_id = ''
+    it 'user_idが空である' do
+      @order_pay_form.user_id = ''
         @order_pay_form.valid?
         expect(@order_pay_form.errors.full_messages).to include("User can't be blank")
       end
@@ -37,8 +44,20 @@ RSpec.describe OrderPayForm, type: :model do
         expect(@order_pay_form.errors.full_messages).to include("Postal code can't be blank")
       end
 
+      it 'postal_codeにハイフンが含まれていない' do
+        @order_pay_form.postal_code = '1111111 '
+        @order_pay_form.valid?
+        expect(@order_pay_form.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+
+      it 'postal_codeの文字数が足りない' do
+        @order_pay_form.postal_code = '11-111'
+        @order_pay_form.valid?
+        expect(@order_pay_form.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+
       it 'prefecture_idがid = 1である' do
-        @order_pay_form.prefecture = '1'
+        @order_pay_form.prefecture = 1
         @order_pay_form.valid?
         expect(@order_pay_form.errors.full_messages).to include("Prefecture must be other than 1")
       end
@@ -54,37 +73,25 @@ RSpec.describe OrderPayForm, type: :model do
         @order_pay_form.valid?
         expect(@order_pay_form.errors.full_messages).to include("Addresses can't be blank")
       end
+
       it 'phone_numberが空である' do
         @order_pay_form.phone_number = ' '
         @order_pay_form.valid?
         expect(@order_pay_form.errors.full_messages).to include("Phone number can't be blank")
       end
-      it 'postal_codeにハイフンが含まれていない' do
-        @order_pay_form.postal_code = '1111111 '
-        @order_pay_form.valid?
-        expect(@order_pay_form.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
-      end
-      it 'postal_codeの文字数が足りない' do
-        @order_pay_form.postal_code = '11-111'
-        @order_pay_form.valid?
-        expect(@order_pay_form.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
-      end
-      
+
       it 'phone_numberの文字数が足りない' do
         @order_pay_form.phone_number = '111111111'
         @order_pay_form.valid?
         expect(@order_pay_form.errors.full_messages).to include( "Phone number is invalid")
       end
-      it "tokenが空では登録できないこと" do
-        @order_pay_form.token = nil
-        @order_pay_form.valid?
-        expect(@order_pay_form.errors.full_messages).to include("Token can't be blank")
-      end
+
       it "phone_numberは英数混合では登録できないこと" do
       @order_pay_form.phone_number = '1a1a1a1a1a1'
       @order_pay_form.valid?
       expect(@order_pay_form.errors.full_messages).to include("Phone number is invalid")
       end
+
     end
   end
 end
